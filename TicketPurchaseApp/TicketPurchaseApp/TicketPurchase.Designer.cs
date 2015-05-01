a428809768c280edfd83a3ec78d160af1e83734d
arrobin <ashiqur007@yahoo.com>
2015-05-01 19:34:29 +0600

    Understanding Update Procedure

---
+++ TicketPurchaseApp/TicketPurchaseApp/TicketPurchase.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TicketPurchaseApp
+{
+    partial class TicketPurchase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.customerNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.noofTicketTextBox = new System.Windows.Forms.TextBox();
+            this.totalPriceButton = new System.Windows.Forms.Button();
+            this.detailsButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // customerNameTextBox
+            // 
+            this.customerNameTextBox.Location = new System.Drawing.Point(136, 28);
+            this.customerNameTextBox.Name = "customerNameTextBox";
+            this.customerNameTextBox.Size = new System.Drawing.Size(200, 20);
+            this.customerNameTextBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "No of Ticket";
+            // 
+            // noofTicketTextBox
+            // 
+            this.noofTicketTextBox.Location = new System.Drawing.Point(136, 64);
+            this.noofTicketTextBox.Name = "noofTicketTextBox";
+            this.noofTicketTextBox.Size = new System.Drawing.Size(200, 20);
+            this.noofTicketTextBox.TabIndex = 1;
+            // 
+            // totalPriceButton
+            // 
+            this.totalPriceButton.Location = new System.Drawing.Point(81, 124);
+            this.totalPriceButton.Name = "totalPriceButton";
+            this.totalPriceButton.Size = new System.Drawing.Size(75, 23);
+            this.totalPriceButton.TabIndex = 2;
+            this.totalPriceButton.Text = "Total Price";
+            this.totalPriceButton.UseVisualStyleBackColor = true;
+            this.totalPriceButton.Click += new System.EventHandler(this.totalPriceButton_Click);
+            // 
+            // detailsButton
+            // 
+            this.detailsButton.Location = new System.Drawing.Point(234, 124);
+            this.detailsButton.Name = "detailsButton";
+            this.detailsButton.Size = new System.Drawing.Size(75, 23);
+            this.detailsButton.TabIndex = 3;
+            this.detailsButton.Text = "Details";
+            this.detailsButton.UseVisualStyleBackColor = true;
+            this.detailsButton.Click += new System.EventHandler(this.detailsButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "One Ticket 10 Taka";
+            // 
+            // TicketPurchase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 196);
+            this.Controls.Add(this.detailsButton);
+            this.Controls.Add(this.totalPriceButton);
+            this.Controls.Add(this.noofTicketTextBox);
+            this.Controls.Add(this.customerNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TicketPurchase";
+            this.Text = "Ticket Purcase UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox customerNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox noofTicketTextBox;
+        private System.Windows.Forms.Button totalPriceButton;
+        private System.Windows.Forms.Button detailsButton;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TicketPurchaseApp/TicketPurchaseApp/TicketPurchase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketPurchaseApp
+{
+    public partial class TicketPurchase : Form
+    {
+
+        private string customerName;
+        private double noofTicket;
+        private double totalPrice;
+        public TicketPurchase()
+        {
+            InitializeComponent();
+        }
+
+        private void totalPriceButton_Click(object sender, EventArgs e)
+        {
+            noofTicket = Convert.ToDouble(noofTicketTextBox.Text);
+
+            totalPrice = noofTicket*10;
+
+            MessageBox.Show("Total Price is " + totalPrice);
+        }
+
+        private void detailsButton_Click(object sender, EventArgs e)
+        {
+            customerName = customerNameTextBox.Text;
+
+            noofTicket = Convert.ToDouble(noofTicketTextBox.Text);
+
+            totalPrice = noofTicket * 10;
+
+            MessageBox.Show("Customer Name = " + customerName + "\n" + "Unit Price = 10" + "\n" + "Number of Ticket = " +
+                            noofTicket + "\n" + "Total Price = " + totalPrice);
+
+        }
+
+       
+    }
+}
